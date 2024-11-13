3e40509558ff9fbbc844ac4821806d54ee78efb4
zou-yongqiu <2667134364@qq.com>
2024-11-13 23:32:01 +0800

    xiaoxian: second do last time commit.

---
+++ .vitepress/layoutSlots/MyLayout.vue
@@ -0,0 +1,11 @@
+<!--.vitepress/theme/MyLayout.vue-->
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+
+const { Layout } = DefaultTheme
+</script>
+
+<!-- 在这里要选择使用的slot -->
+<template #aside-outline-before>
+    <!-- My custom sidebar top content -->
+</template>
